4efd2186f38945e464f82a0e77ab8c50a87b08fc
Daniel Winfield <daniel.winfield@outlook.com>
2022-02-02 11:21:46 +0000

    Moved words to their own file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { GameComponent } from './game/game.component';
 
 @NgModule({
   declarations: [AppComponent, GameComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
